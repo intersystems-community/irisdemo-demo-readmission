072916f096eb8dde8336dd885e2da8dad2b685c5
Phillip Booth <phillip.booth2015@gmail.com>
2020-01-24 12:30:10 -0500

    adding updayes for autobuild

--- RRLACESrv/synthea-loader.sh
+++ RRLACESrv/synthea-loader.sh
@@ -2,7 +2,7 @@
 
 rm -rf output
 
-docker run --rm -v $PWD/output:/output --name synthea-docker intersystemsdc/irisdemo-base-synthea:readmission-demo -p 5000 -s 1577991349289
+docker run --rm -v $PWD/output:/output --name synthea-docker intersystemsdc/irisdemo-base-synthea:readmission-demo -p 5 -s 1577991349289
 
 tar -czvf ./patient-data.tar.gz ./output
 
