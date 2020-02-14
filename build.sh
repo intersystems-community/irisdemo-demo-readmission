fb1386bababd357e2294d3b801d3f244a78a1699
Phillip Booth <phillip.booth2015@gmail.com>
2020-02-14 15:35:53 -0500

    temporarily adding commented out build file

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ DOCKER_REPOSITORY=intersystemsdc/irisdemo-demo-readmission
 
 docker build -t ${DOCKER_REPOSITORY}:risksrv-${DOCKER_TAG} ./image-risksrv/
 
-docker build -t ${DOCKER_REPOSITORY}:riskengine-${DOCKER_TAG} ./image-riskengine/
+#docker build -t ${DOCKER_REPOSITORY}:riskengine-${DOCKER_TAG} ./image-riskengine/
 
-docker build -t ${DOCKER_REPOSITORY}:hisdb-${DOCKER_TAG} ./image-hisdb/
+#docker build -t ${DOCKER_REPOSITORY}:hisdb-${DOCKER_TAG} ./image-hisdb/
 
-docker build -t ${DOCKER_REPOSITORY}:hisui-${DOCKER_TAG} ./image-hisui/
+#docker build -t ${DOCKER_REPOSITORY}:hisui-${DOCKER_TAG} ./image-hisui/
 
